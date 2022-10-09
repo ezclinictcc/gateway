2fdf9a3653d303048fb04b1b5af8edf033367cac
dennisw.dev <dennisw.dev@gmail.com>
2022-10-09 20:37:47 -0300

    Adding new headers in cors

--- src/adapters/external/cors/index.ts
+++ src/adapters/external/cors/index.ts
@@ -19,11 +19,16 @@ const corsAdapter = (
     "Access-Control-Allow-Methods",
     "PUT, POST, GET, DELETE, OPTIONS"
   );
-  response.setHeader("Access-Control-Allow-Headers", "*");
-  response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  // response.setHeader("Access-Control-Allow-Headers", "*");
+  // response.setHeader("Access-Control-Allow-Headers", "Content-Type");
   response.setHeader("Access-Control-Request-Headers", "*");
   response.setHeader("Access-Control-Allow-Credentials", "true");
   response.setHeader("Access-Control-Max-Age", "10");
+
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+  );
 
   if (request.method === "OPTIONS") {
     return response.status(200).end();
