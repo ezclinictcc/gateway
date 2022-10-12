b0c22886e63da0b9e34df2664688f64b3a0c57d9
dennisw.dev <dennisw.dev@gmail.com>
2022-10-12 15:08:20 -0300

    Add token expiration time validation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import httpProxy from "express-http-proxy";
 import swaggerUi from "swagger-ui-express";
-import { decode } from "jsonwebtoken";
+import { decode, verify } from "jsonwebtoken";
 import { getRepositoryAdapter } from "./adapters";
 import corsAdapter from "./adapters/external/cors";
 import ExpressAppConfig from "./adapters/external/express/ExpressAppAdapter";
@@ -34,16 +34,26 @@ app.setMidleware("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.setMidleware(async (req, res, next) => {
   let hasPermissionToAccess = false;
 
-  const needAuth =
-    !req.path.includes("/login") &&
-    !req.path.includes("/user") &&
-    req.method === "POST";
+  const needAuth = !req.path.includes("/login") && !req.path.includes("/user");
 
   const host = selectProxyHost(req);
   const loginController = new LoginController(getRepositoryAdapter());
-
+  console.log("verify auth", needAuth);
   if (needAuth) {
     if (req.headers.authorization) {
+      console.log("Token ", req.headers.authorization);
+
+      verify(
+        req.headers.authorization.replace("Bearer ", ""),
+        "040176b773bf347dced85cfe32038d80",
+        (err) => {
+          if (err) {
+            res.status(401).json({ errorDescription: "token expired" });
+            throw new Error("invalid_jwt");
+          }
+        }
+      );
+
       await loginController
         .find({
           param: {
@@ -54,6 +64,8 @@ app.setMidleware(async (req, res, next) => {
         .then((response) => {
           if (response && response.length === 1) {
             hasPermissionToAccess = true;
+          } else {
+            res.status(403).json({ errorDescription: "access_denied" });
           }
         })
         .catch((error) => {
